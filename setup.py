057a0a5cf6ab5b919c1387c065dbfefbe75795a5
Tal Ben-Nun <tbennun@users.noreply.github.com>
2019-03-04 18:27:24 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='plotmark',
+    version='1.0.0',
+    url='https://github.com/tbennun/plotmark',
+    author='Tal Ben-Nun',
+    author_email='tbennun@gmail.com',
+    description='Plot markers for Matplotlib',
+    packages=find_packages(),
+    install_requires=['matplotlib'])
